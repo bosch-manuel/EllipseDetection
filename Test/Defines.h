89bff1aa81d8b8ef5aa94806b527797554e58279
Manuel Bosch <manuel-s-b@gmx.net>
2014-04-02 18:43:54 +0200

    ...

---
+++ Test/Defines.h
@@ -0,0 +1,18 @@
+#ifndef DEFINES
+#define DEFINES
+
+#define TRUE 1
+#define FALSE 0
+//#define DEBUG
+#define DEBUG_MAIN
+#define EDGE_DEBUG "..\\EdgeLink_Debug.txt"
+#define FINDENDS_DEBUG "..\\FindEnds_Debug.txt"
+#define EDGEPOINT 255
+#define ENDPOINT 200
+#define DEBUG_MAXLINEDEV
+//#define DEBUG_SHOW_LINESEGMENTEDEDGES
+
+#define MIN_LENGTH 10
+#define D_TOL 3
+
+#endif
