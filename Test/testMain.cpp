456fa5c1f65bc46373e6747a857a65bc71c2f58e
Manuel <manuel-s-b@gmx.net>
2014-04-17 16:01:17 +0200

    TestMain

---
+++ Test/testMain.cpp
@@ -0,0 +1,15 @@
+#include "Point.h"
+int main(int argc, char** argv) {
+	Point P(2.5,2);
+	Point L2(1,4.5);
+	Point L1(2,3.5);
+	Point R1(4,3);
+	Point R2(5.5,3.5);
+
+	Point r0 =L1-L2 ;
+	Point r1 = P-L2;
+	Point r2 = R1-L2;
+
+	double al1;
+	double al2;
+}
