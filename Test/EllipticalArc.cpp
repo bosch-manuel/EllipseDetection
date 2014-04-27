e760c19a682126a64f0fb129957f1fa2d10e5e20
Manuel Bosch <manuel-s-b@gmx.net>
2014-04-27 20:02:36 +0200

    ...

---
+++ Test/EllipticalArc.cpp
@@ -0,0 +1,24 @@
+#include "EllipticalArc.h"
+
+EllipticalArc::EllipticalArc()
+:ID(++id_cnt){}
+
+EllipticalArc::~EllipticalArc() {
+	segments.clear();
+}
+
+bool EllipticalArc::containsSegment(EdgeSegment* cS) {
+	return segments.find(cS) != segments.end() ? true : false;
+}
+
+void EllipticalArc::addSegment(EdgeSegment* cS) {
+	segments.insert(cS);
+}
+
+void EllipticalArc::drawToImage(cv::Mat* image, cv::Vec3b color) {
+	for (std::set<EdgeSegment*>::const_iterator i = segments.begin(); i !=segments.end(); i++)	{
+		(*i)->drawToImage(image,color);
+	}
+}
+
+int EllipticalArc::id_cnt = 0;
---
+++ Test/EllipticalArc.h
@@ -0,0 +1,26 @@
+#ifndef ELLIPTICALARC_H
+#define ELLIPTICALARC_H
+
+#include "EdgeSegment.h"
+#include"opencv2\imgproc\imgproc.hpp"
+#include <set>
+
+class EllipticalArc {
+public:
+	EllipticalArc();
+	~EllipticalArc();
+
+	/*true if this elliptical arc contains the specified edge segment */
+	bool containsSegment(EdgeSegment*);
+
+	void addSegment(EdgeSegment*);
+
+	void drawToImage(cv::Mat*, cv::Vec3b);
+
+	int const ID;
+private:
+	std::set<EdgeSegment*> segments;
+	static int id_cnt;
+
+};
+#endif
